3009ee3a8c3af94eeebf09e5a2e169b962a93798
krishna goyal <krishnagoyalpps@gmail.com>
2026-02-27 21:34:01 +0530

    body-parser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 const {PORT} = require('./config/serverConfig.js');
+
 const setupAndStartServer = async () => {
     // create the express object
     const app = express();
+
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({estended: true}));
+
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
     })
 }
+
+
+
 
 setupAndStartServer();
